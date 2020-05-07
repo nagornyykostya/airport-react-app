7f0f548989be35653c310e78d0a5cb0218474ef6
MyMac <nagornyykostya@gmail.com>
2020-05-07 21:47:00 +0300

    airport app init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+// import store from './store.js';
+
+function App() {
+  return (
+    <Provider>
+      <div>Airport</div>
+    </Provider>
+  );
+}
+
+export default App;
