6ab2214c599018d5fd3cd6d6f252db6942ff9404
MyMac <nagornyykostya@gmail.com>
2020-05-12 15:06:27 +0300

    logo import

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "./logo.svg";
+import logo from "./logo.svg";
 
 const Header = () => {
   return (
@@ -8,7 +8,7 @@ const Header = () => {
         <Link className="header-wrapper__logo-container" to="/">
           <img
             className="header-wrapper__logo"
-            src="./logo.svg"
+            src={logo}
             alt="logo"
           />
                   <span>Kyiv Sikorskiy airport</span>
